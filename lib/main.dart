6f6548b9af3dcf0c7ced0979150ba20917f43b44
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-06-03 13:04:25 -0300

    refact: organize code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:poc_bloc/counter_cubit.dart';
+import 'my_home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,75 +17,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  late CounterCubit counterCubit;
-
-  @override
-  void initState() {
-    super.initState();
-    counterCubit = CounterCubit();
-  }
-
-  @override
-  void dispose() {
-    counterCubit.close();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            StreamBuilder<int>(
-                stream: counterCubit.stream,
-                initialData: 0,
-                builder: (_, snapshot) {
-                  return Text(
-                    snapshot.data.toString(),
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  );
-                }),
-          ],
-        ),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          FloatingActionButton(
-            onPressed: counterCubit.increment,
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ),
-          FloatingActionButton(
-            onPressed: counterCubit.decrement,
-            tooltip: 'Decrement',
-            child: const Icon(Icons.remove),
-          ),
-        ],
-      ),
     );
   }
 }
---
+++ lib/my_home_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+import 'counter_cubit.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late CounterCubit counterCubit;
+
+  @override
+  void initState() {
+    super.initState();
+    counterCubit = CounterCubit();
+  }
+
+  @override
+  void dispose() {
+    counterCubit.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            StreamBuilder<int>(
+                stream: counterCubit.stream,
+                initialData: 0,
+                builder: (_, snapshot) {
+                  return Text(
+                    snapshot.data.toString(),
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                }),
+          ],
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          FloatingActionButton(
+            onPressed: counterCubit.increment,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: counterCubit.decrement,
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
